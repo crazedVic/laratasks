2df63f14650f643d43aeef0544e4ac493a548de7
Frenchtoast0 <38544250+Frenchtoast0@users.noreply.github.com>
2021-07-29 10:38:53 -0600

    Started db setup

--- database/migrations/2021_07_29_162806_create_notifications_table.php
+++ database/migrations/2021_07_29_162806_create_notifications_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductsTable extends Migration
+class CreateNotificationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('notifications', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('status');
-            $table->integer('price');
+            $table->unsignedBigInteger('notificationof_id')->nullable();
+            $table->string('notificationof_type')->nullable();
+            $table->string('message');
             $table->timestamps();
         });
     }
@@ -29,6 +29,6 @@ class CreateProductsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('notifications');
     }
 }
