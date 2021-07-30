0ab8bb9633d6b549dfab70f19dfdec9468609bda
Frenchtoast0 <38544250+Frenchtoast0@users.noreply.github.com>
2021-07-30 13:47:26 -0600

    Delete env

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,6 +13,17 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //admin account
+        DB::table('users')->insert([
+            'name' => "Admin",
+            'email' => 'a@b.com',
+            'password' => Hash::make('password'),
+            'email_verified_at' => now(),
+            'created_at' => now(),
+            'updated_at' => now(),
+            'lastlogin_at' => now(),
+            'current_team_id' => 3,
+            'timezone' => 'America/New_York'
+        ]);
     }
 }
